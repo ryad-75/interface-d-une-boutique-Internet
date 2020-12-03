abf6beb53039eb50185cd5e38027a6423c1fe57c
ryad <ryadallaf@gmail.com>
2020-12-04 00:50:31 +0100

    boutique d'images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Cart from "./pages/Cart";
+import Photos from "./pages/Photos";
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Exemlpe Boutique en ligne
-      </h1>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Photos />
+        </Route>
+
+        <Route path="/cart">
+          <Cart />
+        </Route>
+      </Switch>
     </div>
   );
 }
